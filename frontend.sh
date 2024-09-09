d3a3dbe77ff1874b81a7a64c71bb16fbab221ba1
Sindhura <sindhu.tiruvedula@gmail.com>
2024-09-10 00:01:52 +0530

    updated

--- frontend.sh
+++ frontend.sh
@@ -9,54 +9,52 @@ mkdir -p $LOGS_FOLDER
 USERID=$(id -u)
 R="\e[31m"
 G="\e[32m"
-Y="\e[33m"
 N="\e[0m"
+Y="\e[33m"
 
-CHECK_ROOT( ) 
-{
-if [ $USERID -ne 0 ]
-then
-echo -e "$R Please run this script with root priveleges $N" | tee -a $LOG_FILE
-exit1
-fi
+CHECK_ROOT(){
+    if [ $USERID -ne 0 ]
+    then
+        echo -e "$R Please run this script with root priveleges $N" | tee -a $LOG_FILE
+        exit 1
+    fi
 }
 
-VALIDATE()
-{
-if [ $1 -ne 0 ]
-then
-echo -e "$2 is...$R FAILED $N"  | tee -a $LOG_FILE
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 is...$R FAILED $N"  | tee -a $LOG_FILE
         exit 1
     else
-echo -e "$2 is... $G SUCCESS $N" | tee -a $LOG_FILE
-fi
+        echo -e "$2 is... $G SUCCESS $N" | tee -a $LOG_FILE
+    fi
 }
 
-echo -e "$Y script started execution at : $(date) $N"
+echo "Script started executing at: $(date)" | tee -a $LOG_FILE
 
 CHECK_ROOT
 
-dnf install nginx -y 
-VALIDATE $? "Install nginx"
+dnf install nginx -y &>>$LOG_FILE
+VALIDATE $? "Installing Nginx"
 
-systemctl enable nginx 
-VALIDATE $? "Enable nginx"1A
+systemctl enable nginx &>>$LOG_FILE
+VALIDATE $? "Enable Nginx"
 
-systemctl start nginx 
-VALIDATE $? "start nginx"
-    
-rm -rf /usr/share/nginx/html/* 
+systemctl start nginx &>>$LOG_FILE
+VALIDATE $? "Start Nginx"
+
+rm -rf /usr/share/nginx/html/* &>>$LOG_FILE
 VALIDATE $? "Removing default website"
 
 curl -o /tmp/frontend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-frontend-v2.zip &>>$LOG_FILE
 VALIDATE $? "Downloding frontend code"
 
 cd /usr/share/nginx/html
-unzip /tmp/frontend.zip 
+unzip /tmp/frontend.zip &>>$LOG_FILE
 VALIDATE $? "Extract frontend code"
 
 cp /home/ec2-user/Expense-Shell-Project/expense.conf /etc/nginx/default.d/expense.conf
 VALIDATE $? "Copied expense conf"
 
-systemctl restart nginx
+systemctl restart nginx &>>$LOG_FILE
 VALIDATE $? "Restarted Nginx"
