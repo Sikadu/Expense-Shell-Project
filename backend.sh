af69c1531ccfac0db517b2d86bf7d5bafe99cf5b
Sindhura <sindhu.tiruvedula@gmail.com>
2024-09-09 21:43:32 +0530

    updated

--- backend.sh
+++ backend.sh
@@ -67,7 +67,7 @@ unzip /tmp/backend.zip &>>$LOG_FILE
 VALIDATE $? "Extracting backend application code"
 
 npm install &>>$LOG_FILE
-cp /home/ec2-user/expense-shell/backend.service /etc/systemd/system/backend.service
+cp /home/ec2-user/Expense-Shell-Project/backend.service /etc/systemd/system/backend.service
 
 dnf install mysql -y &>>$LOG_FILE
 VALIDATE $? "install mysql"
