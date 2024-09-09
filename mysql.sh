d218fab34fd44077aad84adb4e919ae5d2d428e3
Sindhura <sindhu.tiruvedula@gmail.com>
2024-09-09 21:33:50 +0530

    updated

--- mysql.sh
+++ mysql.sh
@@ -13,7 +13,7 @@ Y="\e[33m"
 N="\e[0m"
 
 CHECK_ROOT( ) {
- if [ $USERID -ne 0 ]
+ if [[$USERID -ne 0 ]]
  then
     echo -e "$R Please run this script with root priveleges $N" | tee -a $LOG_FILE
     exit1
